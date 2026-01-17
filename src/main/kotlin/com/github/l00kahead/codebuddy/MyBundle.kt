bfe3be82db491af8c169ef008cd1d3acc81d9c2d
GitHub Action <action@github.com>
2026-01-17 07:14:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/l00kahead/codebuddy/MyBundle.kt
+++ src/main/kotlin/com/github/l00kahead/codebuddy/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.l00kahead.codebuddy
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/l00kahead/codebuddy/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/l00kahead/codebuddy/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.l00kahead.codebuddy.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
